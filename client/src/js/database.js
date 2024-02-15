8d38b4ea69fac0a2735500f9cb4b49a75a275a31
HackDehZack <Zackseriousemail@gmail.com>
2024-02-15 02:46:11 -0500

    Add database functionality using idb library

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,21 +1,41 @@
+// Importing the openDB function from the 'idb' library
 import { openDB } from 'idb';
 
+// Function to initialize the database
 const initdb = async () =>
   openDB('jate', 1, {
     upgrade(db) {
+      // Checking if the 'jate' object store already exists in the database
       if (db.objectStoreNames.contains('jate')) {
         console.log('jate database already exists');
         return;
       }
+      // Creating the 'jate' object store with auto-incrementing key
       db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
       console.log('jate database created');
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// Function to save text to the database
+export const putDb = async (textEditorContent) => {
+  const jateDB = await openDB('jate', 1);
+  const transaction = jateDB.transaction('jate', 'readwrite');
+  const store = transaction.objectStore('jate');
+  const request = store.put({ jate: textEditorContent });
+  const savedText = await request;
+  console.log('saved text to database;', savedText);
+};
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+// Function to retrieve text from the database
+export const getDb = async () => {
+  const jateDB = await openDB('jate', 1);
+  const transaction = jateDB.transaction('jate', 'readonly');
+  const store = transaction.objectStore('jate');
+  const request = store.get('jate');
+  const retrievedText = await request;
+  console.log('retrieved text from database;', retrievedText);
+  return retrievedText;
+};
 
+// Initializing the database
 initdb();
